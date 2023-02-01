888515d7ef3b57603e5803c51140ea03442c4e24
triciahughes <hughes.a.tricia@gmail.com>
2023-01-31 19:04:25 -0800

    completed lab

--- src/components/BlogContent.js
+++ src/components/BlogContent.js
@@ -1,7 +1,18 @@
 import React from "react";
 
 function BlogContent(props) {
-  return <div id="blog-content">{props.articleText}</div>;
+  console.log(props);
+
+  if (!props.isPublished) {
+    return null;
+  } else {
+    return (
+      <div id="blog-content">
+        <h1>{props.articleText}</h1>
+        <p>{props.minutesToRead}</p>
+      </div>
+    );
+  }
 }
 
 export default BlogContent;
